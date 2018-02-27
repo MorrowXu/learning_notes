d7d202fa9f3477a4dd749d59f1d92994a78d6800
Morrow <464580843@qq.com>
2018-02-27 16:55:40 +0800

    2018-2-27 commit

--- Python高效编程技巧实战_课程笔记_第二章.py
+++ Python高效编程技巧实战_课程笔记_第二章.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 # Author: Morrow
-
-# 第二章数据结构与算法进阶训练
+raise Exception
+# 第二章数据结构与算法进阶训练_笔记
 #----------2-1----------
 # 过滤
 from random import randint
@@ -87,3 +87,75 @@ result = sorted(zip(d.itervalues(), d.iterkeys())) # 方法一,zip后sorted
 sorted(d.items(), key=lambda x: x[1]) # 方法二, 改sorted默认排序参数
 
 #----------2-4----------
+
+#----------2-5----------
+# 快速找到多个字典中的公共键
+from random import randint, sample
+s1 = {x: randint(1,4) for x in sample('abcdefg', randint(3,6))} # sample('abcdefg', randint(3,6)) 随机取样
+s2 = {x: randint(1,4) for x in sample('abcdefg', randint(3,6))}
+s3 = {x: randint(1,4) for x in sample('abcdefg', randint(3,6))}
+
+# 方法一
+res = []
+for k in s1:
+	if k in s2 and k in s3:
+		res.append(k)
+
+# 方法二
+res = s1.viewkeys() & s2.viewkeys() &s3.viewkeys() # 集合交集操作
+
+# 方法三
+reduce(lambda a,b: a&b, map(dict.viewkeys(), [s1,s2,s3]))  # map得到每轮的集合,再用reduce进行与运算操作
+
+#----------2-5----------
+
+#----------2-6----------
+# 如何让字典保持有序
+from collections import OrderedDict
+from time import time
+d = OrderedDict()
+players = list('ABCDEFGH')
+start = time()
+
+for i in xrange(8):
+	raw_input()
+	p = players.pop(randint(0, 7-i))
+	end = time()
+	print i + 1, p, end - start
+	d[p] = (i + 1, end - start)
+
+#----------2-6----------
+
+#----------2-7----------
+# 实现用户的历史记录功能
+from collections import deque # 双端队列
+
+N = randint(0, 100)
+history = deque([], 5)
+
+def guess(K):
+	if K == N:
+		print 'right'
+		return True
+	if K < N:
+		print '%s is less-than N' % K
+	else:
+		print '%s is greater-than N' % K
+	return False
+
+while True:
+	line = raw_input('please input a number: ')
+	if line.isdigit():
+		K = int(line)
+		history.append(K)
+		if guess(K):
+			break
+	elif line == 'history' or line == 'h?':
+		print list(history)
+# 对象存储
+import pickle
+pickle.dump(q, open('history', 'w'))
+q2 = pickle.load(open('history'))
+
+
+#----------2-7----------
